b35d26af33f0c2d3495a57e417c16bd398a06a2c
Sanghapal Shelke <sanghapal25@gmail.com>
2018-11-10 14:15:39 +0000

    Update CoinMarketCap_clone.py

--- CoinMarketCap_clone.py
+++ CoinMarketCap_clone.py
@@ -3,3 +3,6 @@ from prettytable import PrettyTable
 listings_api = 'https://api.coinmarketcap.com/v2/listings/'
 ticker_api = 'https://api.coinmarketcap.com/v2/ticker/?start='
 listings_data = requests.get(listings_api).json()['data']
+
+table = PrettyTable()
+table.field_names = ['Name','Symbol','Price','Volume','MarketCap','Change 1h','Change 24h','Change 7d']
