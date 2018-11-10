f80ddadeb290fe5cb02c8da41cb90b9a3534650e
Sanghapal Shelke <sanghapal25@gmail.com>
2018-11-10 14:16:04 +0000

    Update CoinMarketCap_clone.py

--- CoinMarketCap_clone.py
+++ CoinMarketCap_clone.py
@@ -6,3 +6,29 @@ listings_data = requests.get(listings_api).json()['data']
 
 table = PrettyTable()
 table.field_names = ['Name','Symbol','Price','Volume','MarketCap','Change 1h','Change 24h','Change 7d']
+
+nr_coins = 0
+
+while nr_coins < len(listings_data):
+
+    temp_ticker_api = ticker_api + str(nr_coins)
+    ticker_data = requests.get(temp_ticker_api).json()
+    ticker_data = ticker_data['data']
+
+    for coin in ticker_data:
+        name = ticker_data[coin]['name']
+        symbol = ticker_data[coin]['symbol']
+        coin = ticker_data[coin]['quotes']['USD']
+
+        table.add_row([ name,
+                        symbol,
+                        coin['price'],
+                        coin['volume_24h'],
+                        coin['market_cap'],
+                        coin['percent_change_1h'],
+                        coin['percent_change_24h'],
+                        coin['percent_change_7d']])
+
+    nr_coins += 100
+
+print(table)
